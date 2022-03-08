bd0d5d860486c923be0c7c3a0069314cf1c9d209
silvinaarguello <silvinaarguello@gmail.com>
2022-03-08 16:10:56 -0300

    first

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = prueba silvi;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 10;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 20>10;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - null === 5;
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
